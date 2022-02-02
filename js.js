37ca86f4901f5f27b268ba0cee459bd86a4a375e
Earl Adams <earladams2000@gmail.com>
2022-02-02 13:43:52 -0500

    script changes

--- js.js
+++ js.js
@@ -4,6 +4,31 @@ console.log(date.format("dddd, MMMM Do"));
 //puts date and time on screen
 $("#currentDay").text(date.format("[Hello, today is ]dddd, MMMM Do"));
 $("#currentTime").text(date.format("[The current time is ]h:mma"));
-
-
 //calendar format
+
+$(".saveBtn")
+console.log($(".saveBtn"))
+function testFunction(name) {
+    console.log(name)
+}
+testFunction("Adams")
+
+var buttonArray = $(".saveBtn");
+ for (let i = 0; i < buttonArray.length; i++) {
+     //console.log(buttonArray[i]);
+     var storageHr = i + 9  
+     var savedLocal = localStorage.getItem("#hr" + storageHr); 
+     var inputArea = "#hr" + storageHr    
+    $(inputArea).val(savedLocal)
+     console.log(localStorage.getItem("#hr" + storageHr));
+     $(buttonArray[i]).click(function(event) {
+         event.preventDefault();
+        console.log(event.target.value);
+        var hourId = "#" + event.target.value
+        console.log($(hourId).val());
+    var inputVal = $(hourId).val(); 
+    localStorage.setItem(hourId , inputVal )
+
+      });
+
+ }
