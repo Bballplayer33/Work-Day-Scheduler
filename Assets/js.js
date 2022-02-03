1104532b41a527519af0398a2ef1cf7b3fec2b72
Earl Adams <earladams2000@gmail.com>
2022-02-02 22:44:12 -0500

    Main changes to code/structure

--- Assets/js.js
+++ Assets/js.js
@@ -4,25 +4,28 @@ console.log(date.format("dddd, MMMM Do"));
 //puts date and time on screen
 $("#currentDay").text(date.format("[Hello, today is ]dddd, MMMM Do"));
 $("#currentTime").text(date.format("[The current time is ]h:mma"));
-//calendar format
 
+//j query save button 
+//test
 $(".saveBtn")
 console.log($(".saveBtn"))
 function testFunction(name) {
     console.log(name)
 }
 testFunction("Adams")
-
+//Getting all buttons using for loop
 var buttonArray = $(".saveBtn");
  for (let i = 0; i < buttonArray.length; i++) {
      //console.log(buttonArray[i]);
+     //array of 9
      var storageHr = i + 9  
      var savedLocal = localStorage.getItem("#hr" + storageHr);
-     var inputArea = "#hr" + storageHr    
+     var inputArea = "#hr" + storageHr  
+     //setting up local storage  
     $(inputArea).val(savedLocal)
      console.log(localStorage.getItem("#hr" + storageHr));
 
-
+        //setting up save button not letting pg refresh
      $(buttonArray[i]).click(function(event) {
          event.preventDefault();
         console.log(event.target.value);
